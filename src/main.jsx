673346b05ca8dfd9137773cd39c321085e4350a7
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-01-15 03:01:26 +0800

    setup redux toolkit; done set and remove user slice

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,20 @@ import './index.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+const persistor = persistStore(store);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <>
-    <App />
-    <ToastContainer />
-  </>,
-  // </React.StrictMode>,
+  <Provider store={store}>
+    {/* <React.StrictMode> */}
+    <PersistGate persistor={persistor}>
+      <App />
+      <ToastContainer />
+    </PersistGate>
+    {/* </React.StrictMode>, */}
+  </Provider>
 );
