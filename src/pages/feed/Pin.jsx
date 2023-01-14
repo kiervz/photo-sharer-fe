5f60397a23fe892dc287b9ddf4db981cee48c22e
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-01-14 12:15:42 +0800

    add react icons

--- src/pages/feed/Pin.jsx
+++ src/pages/feed/Pin.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
+import { BiDownvote, BiUpvote, BiCommentDetail } from 'react-icons/bi';
+
 import { Link } from 'react-router-dom';
 
-const Pin = ({ id, description, photo, total_votes }) => {
+const Pin = ({ id, description, photo, total_votes, comments }) => {
   return (
     <div className='mt-2 mb-10'>
       <div className='relative w-auto rounded-lg overflow-hidden transition-all duration-500 ease-in-out m-3'>
+        <div>
+          
+        </div>
         <Link to={`/posts/${id}`}>
           <img className='rounded-lg w-full cursor-pointer' src={photo} alt='photo' />
         </Link>
-        <div className='p-2 text-sm flex justify-between items-start gap-3'>
-          <p>{ description }</p>
-          <div className='flex flex-col items-center justify-center p-1'>
-            <p>+</p>
-            <p>{ total_votes }</p>
-            <p>-</p>
+        <div className='p-2 '>
+          <div className='flex items-center text-center justify-between'>
+            <div className='flex justify-center gap-2'>
+              <BiUpvote 
+                className='text-2xl cursor-pointer hover:text-red-500' 
+              />
+              <p className='text-lg font-semibold'>{ total_votes }</p>
+              <BiDownvote 
+                className='text-2xl cursor-pointer hover:text-gray-500' 
+              />
+            </div>
+            <div className='flex items-center text-center justify-between gap-2'>
+              <p className='text-lg font-semibold'>{ comments.total }</p>
+              <BiCommentDetail 
+                className='text-2xl cursor-pointer hover:text-blue-500'
+              />
+            </div>
           </div>
+        </div>
+        <div className='px-2 pb-2 text-sm gap-1'>
+          <p>{ description }</p>
         </div>
       </div>
     </div>
