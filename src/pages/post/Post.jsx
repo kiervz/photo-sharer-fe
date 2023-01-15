a2efc9b9f40f54590a27d1fec88f0005a1ba3c63
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-01-16 00:16:34 +0800

    done delete post

--- src/pages/post/Post.jsx
+++ src/pages/post/Post.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
+
 import { BiDownvote, BiUpvote, BiCommentDetail } from 'react-icons/bi';
-import { HiDotsVertical, HiOutlinePencilAlt, HiOutlineTrash } from 'react-icons/hi';
+import { HiDotsVertical, HiOutlineExclamationCircle, HiOutlinePencilAlt, HiOutlineTrash } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { useSelector } from 'react-redux';
+import { Button, Modal } from 'flowbite-react';
 
 import UserIcon from '../../assets/images/user.png';
 
@@ -13,12 +15,17 @@ const Post = ({
   description, 
   photo, 
   total_votes, 
-  comments 
+  comments,
+  handleDelete
 }) => {
   let kebabRef = useRef();
-  const [isOpen, setIsOpen] = useState(false);
   const userState = useSelector(state => state.user);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
+  const handleDeletePost = () => {
+    handleDelete(id);
+  };
   useEffect(() => {
     document.addEventListener('mousedown', (e) => {
       if (!kebabRef?.current?.contains(e.target)) {
@@ -50,16 +57,46 @@ const Post = ({
                     <HiOutlinePencilAlt size={20} />
                     Edit
                   </Link>
-                  <li >
+                  <li onClick={() => setIsOpenModal(true)} >
                     <p
                       className="cursor-pointer px-4 py-2 text-black hover:bg-gray-100 flex gap-2 justify-start items-center"
                     > 
                       <HiOutlineTrash size={20} />
-                    Delete
+                      Delete
                     </p>
                   </li>
                 </ul>
               </div>
+              <Modal
+                show={isOpenModal}
+                size="md"
+                popup={true}
+                onClose={() => setIsOpenModal(false)}
+              >
+                <Modal.Header />
+                <Modal.Body>
+                  <div className="text-center">
+                    <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                      Are you sure you want to delete this Post?
+                    </h3>
+                    <div className="flex justify-center gap-4">
+                      <Button
+                        color="failure"
+                        onClick={handleDeletePost}
+                      >
+                        {'Yes, I\'m sure'}
+                      </Button>
+                      <Button
+                        color="gray"
+                        onClick={() => setIsOpenModal(false)}
+                      >
+                        No, cancel
+                      </Button>
+                    </div>
+                  </div>
+                </Modal.Body>
+              </Modal>
             </> }
         </div>
         <Link to={`/post/${id}`}>
