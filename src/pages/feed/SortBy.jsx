2c51c9202fd028c956217ce3bb8620a087832962
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-01-16 15:58:53 +0800

    refactor sort by and fix double fetch post on first load

--- src/pages/feed/SortBy.jsx
+++ src/pages/feed/SortBy.jsx
@@ -1,39 +1,36 @@
-import { Dropdown } from 'flowbite-react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const SortBy = ({ handleSelectedSort }) => {
-  const handleSelectSortBy = (sort) => {
+const SORT_BY = [
+  { value: 'latest', label: 'Latest' },
+  { value: 'oldest', label: 'Oldest' },
+  { value: 'highest-votes', label: 'Highest Votes' },
+  { value: 'lowest-votes', label: 'Lowest Votes' },
+];
+
+const SortBy = ({ handleSelectedSort, className }) => {
+  const [sort, setSort] = useState('latest');
+
+  useEffect(() => {
     handleSelectedSort(sort);
-  };
+  }, [sort]);
 
   return (
-    <div className='flex justify-end'>
-      <Dropdown
-        color='gray'
-        label="Sort By"
-        dismissOnClick={false}
+    <div className={`${className} flex justify-end`}>
+      <select 
+        id='sort' 
+        className='cursor-pointer bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-max p-2.5'
+        value={sort}
+        onChange={(e) => setSort(e.target.value)}
       >
-        <Dropdown.Item
-          onClick={() => handleSelectSortBy('latest')}
-        >
-          Latest
-        </Dropdown.Item>
-        <Dropdown.Item
-          onClick={() => handleSelectSortBy('oldest')}
-        >
-          Oldest
-        </Dropdown.Item>
-        <Dropdown.Item
-          onClick={() => handleSelectSortBy('highest-votes')}
-        >
-          Highest Votes
-        </Dropdown.Item>
-        <Dropdown.Item
-          onClick={() => handleSelectSortBy('lowest-votes')}
-        >
-          Lowest Votes
-        </Dropdown.Item>
-      </Dropdown>
+        { SORT_BY.map(sort => (
+          <option 
+            value={sort.value} 
+            key={sort.value}
+          >
+            {sort.label}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
